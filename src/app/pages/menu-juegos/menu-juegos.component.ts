a5545f7a823fc15958a652b1452a3342f5be5cc3
ariel <gonzalez.r.ariel@gmail.com>
2022-06-10 10:58:10 -0300

    update

--- src/app/pages/menu-juegos/menu-juegos.component.ts
+++ src/app/pages/menu-juegos/menu-juegos.component.ts
@@ -1,8 +1,7 @@
 
 import { Component,OnInit } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { USER_STORAGE_KEY } from '@shared/constants/constant';
-import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-menu-juegos',
